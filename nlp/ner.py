f3d2eb50ea97f9b33cee3b0923377afb226837e6
Reinhard <reinhard@192.168.1.2>
2023-09-24 19:06:13 +0700

    fixed company names list

--- nlp/ner.py
+++ nlp/ner.py
@@ -1,9 +1,10 @@
 import re
+import pandas as pd
 
-def extract_company_mentions(text, company_data):
+def extract_company_mentions(text, df_company):
     company_mentions = []
-    for _, row in company_data.iterrows():
-        company_name = row['Company Name']
+    for _, row in df_company.iterrows():
+        company_name = row['Company Name'][:-1] # delete dot from the end of Tbk.
         ticker = row['Code']
         
         # Regular expression pattern to match the company name in the text (case-insensitive)
@@ -13,7 +14,7 @@ def extract_company_mentions(text, company_data):
             start_pos = match.start()
             end_pos = match.end()
             # company_id = company_name.replace(" ", "_").lower()  # Generating a simple company ID.
-            company_mentions.append((start_pos, end_pos, company_name))
+            company_mentions.append((start_pos, end_pos, company_name, ticker))
         
         # Exact match for ticker (case-sensitive with word boundaries)
         ticker_pattern = r'\b{}\b'.format(re.escape(ticker))
@@ -21,6 +22,8 @@ def extract_company_mentions(text, company_data):
             start_pos = match.start()
             end_pos = match.end()
             company_id = ticker  # Generating a simple company ID from the ticker.
-            company_mentions.append((start_pos, end_pos, company_id))
-    
-    return company_mentions
+            company_mentions.append((start_pos, end_pos, company_id, ticker))
+
+    json_mentions = [{'start': start, 'end': end, 'text': text, 'ticker': ticker} for start, end, text, ticker in company_mentions]
+
+    return json_mentions
